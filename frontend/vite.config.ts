a8df5156b1612368429a507b5fff0804a410367f
Sadiq Shahid <sadiq.shahid101@gmail.com>
2026-04-03 19:00:38 -0300

    Add sitemap config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        tailwindcss()
+        tailwindcss(),
+        sitemap({
+            hostname: 'https://runcalcs.ca',
+            dynamicRoutes: [
+                '/pace', '/units', '/wa-points', '/splits',
+                '/steeple-laps', '/events', '/flat-banked-track',
+                '/elevation-pace', '/about'
+            ],
+        }),
     ],
 })
